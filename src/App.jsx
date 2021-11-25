08242f152c2006880138311ff9f69f5198ec7dca
creator-krixus <wilsonrueda2@gmail.com>
2021-11-25 13:00:01 -0500

    Feature: creation login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+import Footer from "./components/footer/Footer";
 import Header from './components/header/Header';
+import Login from "./components/login/Login";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+     <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
