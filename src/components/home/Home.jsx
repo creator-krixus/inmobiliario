8074b37542d8700fb9374a2db8310c038ab9774e
CESAR ANDRES ABADIA DAVILA <ceanabad@gmail.com>
2021-11-29 10:34:30 -0500

    Feature: About creation

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,51 +1,8 @@
 import React from 'react'
-<<<<<<< HEAD
-import About from '../about/About'
-export default function Home() {
-    return (
-        <div> 
-            <About  />
-        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="https://i.pinimg.com/736x/78/34/d3/7834d3bb14d0bb3f453257d8190d57da--painted-brick-houses-green-house-black-shutters.jpg" class="d-block w-100" width="304" height="570" alt="Casas" />
-                <div class="carousel-caption d-none d-md-block">
-                  <h5>First slide label</h5>
-                  <p>Some representative placeholder content for the first slide.</p>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <img src="https://static7.depositphotos.com/1041088/758/i/600/depositphotos_7589448-stock-photo-large-beige-house-with-green.jpg" class="d-block w-100" width="304" height="570" alt="Apartamentos" />
-                <div class="carousel-caption d-none d-md-block">
-                  <h5>Second slide label</h5>
-                  <p>Some representative placeholder content for the second slide.</p>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <img src="https://st2.depositphotos.com/1041088/7766/i/600/depositphotos_77665926-stock-photo-large-modern-house-with-stone.jpg" class="d-block w-100" width="304" height="570" alt="Locales" />
-                <div class="carousel-caption d-none d-md-block">
-                  <h5>Third slide label</h5>
-                  <p>Some representative placeholder content for the third slide.</p>
-                </div>
-              </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-            </button>
-=======
 import CustomerExperience from '../customers/CustomerExperience'
 import Properties from '../properties/Properties'
 import Slider from '../slider/Slider'
+import About from '../about/About'
 import Title from '../titulo/Title'
 
 
@@ -53,11 +10,11 @@ export default function Home() {
     return (
       <div>
           <Slider />
+          <About />
           <Title title="Inmuebles BREXIU"/>
           <Properties />
           <Title title="Experiencias BREXIU" />
           <CustomerExperience />
->>>>>>> 4aa1a88aeec2670401ded74420df9d0df1e356fa
       </div>
-      </div>
+     
     )}
