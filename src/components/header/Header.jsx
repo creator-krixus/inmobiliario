ebd98088a04fa267802f4327fc4a5bcb4da21279
creator-krixus <wilsonrueda2@gmail.com>
2021-12-02 22:21:21 -0500

    Feature: modified footer

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import logo from '../../images/logo.png'
 export default function Header() {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-light fw-bold" style={{background:'#EAEDED '}}>
                 <div className="container-fluid">
                 <Link className="navbar-brand" to="/"><img src={logo} width="90" height="60" alt="Brexiu logo"/><strong>Grupo Brexiu</strong></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
