769126b9c57ff07142b81479eb044e53c3e8f082
CESAR ANDRES ABADIA DAVILA <ceanabad@gmail.com>
2021-12-08 20:39:04 -0500

    Feature: Inverdiones page done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Login from './components/login/Login';
+import Inversiones from './components/inversiones/Inversiones';
 import PageNotFound from './components/pageNotFound/PageNotFound';
 import Home from './components/home/Home';
 import SignUp from './components/register/SignUp';
@@ -9,6 +10,7 @@ import Separator from './components/separator/Separator';
 import MisionVision from './components/misionVision/MisionVision'
 import './App.css';
 import ShowProperti from "./components/showPropertie/ShowProperti";
+
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="login" element={<Login/>} />
+          <Route path="inversiones" element={<Inversiones/>} />
           <Route path="register" element={<SignUp/>} />
           <Route path="misionvision" element={<MisionVision/>} />
           <Route path="apartamento" element={<ShowProperti/>} />
--- src/components/customerInversion/CustomerInversion.jsx
+++ src/components/customerInversion/CustomerInversion.jsx
@@ -6,13 +6,13 @@ export default function CustomerInversion() {
     return (
         <div className="CustomerInversion">
               <div className="ContenedorBtn">
-                <img className="Customer" src="https://cdn.pixabay.com/photo/2019/10/02/14/23/house-keys-4521073_960_720.jpg" alt="" /> <br />
-                <Link className="btnInversion" to="#">CONOCE MÁS</Link>
+                <img className="Customer" src="https://cdn.pixabay.com/photo/2019/10/02/14/23/house-keys-4521073_960_720.jpg" alt="" width="100%" /> <br />
+                <Link class="btnInversion" to="#">CONOCE MÁS</Link>
               </div> 
               
               <div className="ContenedorBtn">
-                <img className="Customer" src="https://blog.marivalresorts.com/media/uploads/2016/07/blog_Fe.jpg" alt="" /> <br /> 
-                <Link className="btnInversion" to="#">CONOCE MÁS</Link>
+                <img className="Customer" src="https://blog.marivalresorts.com/media/uploads/2016/07/blog_Fe.jpg" alt="" width="100%" /> <br /> 
+                <Link class="btnInversion" to="#">CONOCE MÁS</Link>
               </div>  
             
         </div>
